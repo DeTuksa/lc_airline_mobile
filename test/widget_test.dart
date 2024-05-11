80676b42300310fa72f5ac866a5a6d2ad77d357b
DeTuksa <emmydave414@gmail.com>
2024-05-11 14:27:05 +0100

    Test: Booking scren test case

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lc_airline/core/presentation/constants/app_assets.dart';
+import 'package:lc_airline/features/booking/view/booking_screen.dart';
 import 'package:lc_airline/features/booking/view/widgets/flight_card.dart';
 
 void main() {
@@ -22,5 +23,26 @@ void main() {
 
     // Verify if the Divider widget is rendered
     expect(find.byType(Divider), findsOneWidget);
+  });
+
+  testWidgets('BookingScreen widget test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: BookingScreen(),
+    ));
+
+    // Verify if the BookingScreen widget is rendered
+    expect(find.byType(BookingScreen), findsOneWidget);
+
+    // Verify if the Scaffold widget is rendered
+    expect(find.byType(Scaffold), findsOneWidget);
+
+    // Verify if the SingleChildScrollView widget is rendered
+    expect(find.byType(SingleChildScrollView), findsOneWidget);
+
+    // Verify if the Column widget is rendered
+    expect(find.byType(Column), findsNWidgets(4));
+
+    // Verify if three FlightCard widgets are rendered
+    expect(find.byType(FlightCard), findsNWidgets(3));
   });
 }
